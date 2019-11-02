60ee9898643eb4bc23fc5c1b83fb65b7df195d24
AlexeySub <mr.317676@gmail.com>
2019-11-02 15:45:26 +0300

    cors

--- hackaton/settings.py
+++ hackaton/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3030',
+    'http://localhost:3000',
 ]
 
 # Static files (CSS, JavaScript, Images)
